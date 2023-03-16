6740b80a8bab398f800808b996f2dad55c674016
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-03-16 14:56:32 +0100

    Add test for the factorial method

---
+++ spec/solver_spec.rb
@@ -0,0 +1,17 @@
+require './solver'
+
+describe Solver do
+    describe "#factorial" do
+    it "returns the factorial of a positive integer" do
+      expect(subject.factorial(5)).to eq(120)
+    end
+
+    it "returns 1 for 0" do
+      expect(subject.factorial(0)).to eq(1)
+    end
+
+    it "raises an exception for negative integers" do
+      expect { Factorial.factorial(-5) }.to raise_error(ArgumentError)
+    end
+  end
+end
