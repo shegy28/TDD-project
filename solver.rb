91ab8a39e34e9b95fd171a6f121123a7cea37e40
willy836 <wilsonkimiri836@gmail.com>
2023-03-16 17:28:07 +0300

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,26 +1,25 @@
 class Solver
-    def factorial(n)
-      raise ArgumentError, "Argument must be non-negative" if n < 0
-  
-      result = 1
-      (1..n).each { |i| result *= i }
-      result
-    end
+  def factorial(num)
+    raise ArgumentError, 'Argument must be non-negative' if num.negative?
 
-    def reverse(word)
-        word.reverse
-    end
-
-    def fizzbuzz(num)
-      if num % 3 == 0 && num % 5 == 0
-        "fizzbuzz"
-      elsif num % 3 == 0
-        "fizz"
-      elsif num % 5 == 0
-        "buzz"
-      else
-        num.to_s
-      end
-    end
-    
+    result = 1
+    (1..num).each { |i| result *= i }
+    result
   end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
+end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,47 +1,47 @@
 require './solver'
 
 describe Solver do
-    describe "#factorial" do
-        it "returns the factorial of a positive integer" do
-        expect(subject.factorial(5)).to eq(120)
-        end
-        
-        it "returns 1 for 0" do
-        expect(subject.factorial(0)).to eq(1)
-        end
-
-        it "raises an exception for negative integers" do
-        expect { subject.factorial(-5) }.to raise_error(ArgumentError)
-        end
+  describe '#factorial' do
+    it 'returns the factorial of a positive integer' do
+      expect(subject.factorial(5)).to eq(120)
     end
 
-    describe "#reverse" do
-        it "returns the reversed word" do
-            expect(subject.reverse("hello")).to eq("olleh")
-            expect(subject.reverse("world")).to eq("dlrow")
-            expect(subject.reverse("12345")).to eq("54321")
-        end
+    it 'returns 1 for 0' do
+      expect(subject.factorial(0)).to eq(1)
     end
 
-    describe "#fizzbuzz" do
+    it 'raises an exception for negative integers' do
+      expect { subject.factorial(-5) }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#reverse' do
+    it 'returns the reversed word' do
+      expect(subject.reverse('hello')).to eq('olleh')
+      expect(subject.reverse('world')).to eq('dlrow')
+      expect(subject.reverse('12345')).to eq('54321')
+    end
+  end
+
+  describe '#fizzbuzz' do
     it "returns 'fizz' when given a multiple of 3" do
-      expect(subject.fizzbuzz(3)).to eq("fizz")
-      expect(subject.fizzbuzz(6)).to eq("fizz")
+      expect(subject.fizzbuzz(3)).to eq('fizz')
+      expect(subject.fizzbuzz(6)).to eq('fizz')
     end
 
     it "returns 'buzz' when given a multiple of 5" do
-      expect(subject.fizzbuzz(5)).to eq("buzz")
-      expect(subject.fizzbuzz(10)).to eq("buzz")
+      expect(subject.fizzbuzz(5)).to eq('buzz')
+      expect(subject.fizzbuzz(10)).to eq('buzz')
     end
 
     it "returns 'fizzbuzz' when given a multiple of 3 and 5" do
-      expect(subject.fizzbuzz(15)).to eq("fizzbuzz")
-      expect(subject.fizzbuzz(30)).to eq("fizzbuzz")
+      expect(subject.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(subject.fizzbuzz(30)).to eq('fizzbuzz')
     end
 
-    it "returns the given number as a string in any other case" do
-      expect(subject.fizzbuzz(7)).to eq("7")
-      expect(subject.fizzbuzz(11)).to eq("11")
+    it 'returns the given number as a string in any other case' do
+      expect(subject.fizzbuzz(7)).to eq('7')
+      expect(subject.fizzbuzz(11)).to eq('11')
     end
   end
 end
