aad4fad4f31f4950a51455b04ca39972e59cc482
willy836 <wilsonkimiri836@gmail.com>
2023-03-16 17:22:05 +0300

    Fix typo in spec solver_spec file

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -25,23 +25,23 @@ describe Solver do
 
     describe "#fizzbuzz" do
     it "returns 'fizz' when given a multiple of 3" do
-      expect(solver.fizzbuzz(3)).to eq("fizz")
-      expect(solver.fizzbuzz(6)).to eq("fizz")
+      expect(subject.fizzbuzz(3)).to eq("fizz")
+      expect(subject.fizzbuzz(6)).to eq("fizz")
     end
 
     it "returns 'buzz' when given a multiple of 5" do
-      expect(solver.fizzbuzz(5)).to eq("buzz")
-      expect(solver.fizzbuzz(10)).to eq("buzz")
+      expect(subject.fizzbuzz(5)).to eq("buzz")
+      expect(subject.fizzbuzz(10)).to eq("buzz")
     end
 
     it "returns 'fizzbuzz' when given a multiple of 3 and 5" do
-      expect(solver.fizzbuzz(15)).to eq("fizzbuzz")
-      expect(solver.fizzbuzz(30)).to eq("fizzbuzz")
+      expect(subject.fizzbuzz(15)).to eq("fizzbuzz")
+      expect(subject.fizzbuzz(30)).to eq("fizzbuzz")
     end
 
     it "returns the given number as a string in any other case" do
-      expect(solver.fizzbuzz(7)).to eq("7")
-      expect(solver.fizzbuzz(11)).to eq("11")
+      expect(subject.fizzbuzz(7)).to eq("7")
+      expect(subject.fizzbuzz(11)).to eq("11")
     end
   end
 end
